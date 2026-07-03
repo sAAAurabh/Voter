e46eb89807138d397d2cfc88f93798ceadf85776
Saurabh <saurabvhadhik@gmail.com>
2026-07-03 23:31:16 +0545

    test

--- admin.cpp
+++ admin.cpp
@@ -15,7 +15,8 @@ using namespace std;
 // generates salt
 string Admin::gen_salt()
 {
-    string chars = "abcdefghijklmnopqrstuvwxyz0123456789 saurabh added this rn test test test";
+    string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    string c = "hello";
     string salt;
     int i ;
     for (i = 0; i < 6; i++)
