92ce3ec95915d38947bd13bc51680defd406f913
Saurabh <saurabvhadhik@gmail.com>
2026-07-03 23:01:16 +0545

    hello

--- admin.cpp
+++ admin.cpp
@@ -15,7 +15,7 @@ using namespace std;
 // generates salt
 string Admin::gen_salt()
 {
-    string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    string chars = "abcdefghijklmnopqrstuvwxyz0123456789 saurabh added ";
     string salt;
 
     for (int i = 0; i < 6; i++)
