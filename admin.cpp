f4dd2af67fc5196a12ffd57da844f4c7bdbab336
Saurabh <saurabvhadhik@gmail.com>
2026-07-03 23:34:10 +0545

    test successful

--- admin.cpp
+++ admin.cpp
@@ -16,7 +16,6 @@ using namespace std;
 string Admin::gen_salt()
 {
     string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
-    string c = "hello";
     string salt;
     int i ;
     for (i = 0; i < 6; i++)
