fdc09f875733896e9354e9007cc3e3214df51a39
sAAAurabh <saurabvhadhik@gmail.com>
2026-07-11 21:00:35 +0545

    Update admin.cpp

--- admin.cpp
+++ admin.cpp
@@ -16,6 +16,8 @@ using namespace std;
 string Admin::gen_salt()
 {
     string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    string salt;
+    int i ;
     for (i = 0; i < 6; i++)
         salt += chars[rand() % chars.size()];
 
