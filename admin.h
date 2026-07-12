b74d6bc2e3272f0e1af68e4b58b38fafa0c9bedb
Saurabh <saurabvhadhik@gmail.com>
2026-07-12 13:15:49 +0545

    asd

--- admin.h
+++ admin.h
@@ -62,8 +62,4 @@ public:
 
 };
 
-<<<<<<< Updated upstream
 
-
-=======
->>>>>>> Stashed changes
--- voter_home.h
+++ voter_home.h
@@ -34,19 +34,9 @@ private:
 
     QString voter_nid;
 
-<<<<<<< Updated upstream
+
 signals:
     void logout_requested();
     void vote_page_requested(QString nid);
     void candidate_view_requested();
     void result_page_requested();
-
-=======
-
-private slots:
-    void logout();
-    void vote();
-    void view_candidates();
-
->>>>>>> Stashed changes
-};
