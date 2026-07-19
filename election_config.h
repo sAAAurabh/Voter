f94e3c19c018e005446a34f397ce6076b8c0783a
Saurabh <saurabvhadhik@gmail.com>
2026-07-19 15:16:14 +0545

    change

--- election_config.h
+++ election_config.h
@@ -18,7 +18,7 @@ public:
         return QDateTime(QDate(2026, 7, 7), QTime(0, 0));
     }
     static QDateTime registrationEnd() {
-        return QDateTime(QDate(2026, 7, 13), QTime(23, 59, 59));
+        return QDateTime(QDate(2026, 7, 19), QTime(23, 59, 59));
     }
 
 
